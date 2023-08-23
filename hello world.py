ff45c8dec626f0d4b44a39b6db7bbefcd6e145cf
Kauanpl <142547950+Kauanpl@users.noreply.github.com>
2023-08-24 11:36:07 +1200

    Add files via upload

--- hello world.py
+++ hello world.py
@@ -1,1 +1,14 @@
+# code to learn about variables
 
+# variables can be string
+greeting = "hello world"
+
+# they can also be number
+fav_num = 12
+
+# this is variable has a problem
+my_name = "Kauan"
+
+print(greeting)
+print(fav_num)
+print(my_name)
