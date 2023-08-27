6019479f01f23185c09ace5d285fe28132a2d615
Kauanpl <142547950+Kauanpl@users.noreply.github.com>
2023-08-28 09:12:27 +1200

    Add files via upload

---
+++ getting_input.py
@@ -0,0 +1,10 @@
+# Ask the user for their name
+
+# Ask the user for their favourite integer
+
+# Double, halve and square the number
+
+# Greet the user
+
+# Output the results of doubling, halving
+# and squaring their favourite number
